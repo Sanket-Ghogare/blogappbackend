f623525a4900f8995d0df0036a6bd315417678b4
sanket <sanketvghogare@gmail.com>
2025-03-23 22:49:13 +0530

    final commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port =5000;
 
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/akki').then(()=>{
+mongoose.connect('mongodb://127.0.0.1:27017/newdb').then(()=>{
     console.log("connected to mongoDb");
 }).catch((error)=>{
     console.log("Mogodb is failed to connect",error);
